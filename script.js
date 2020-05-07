6010e9e13f64b16a9e90b00bb79361e82323fece
Rayhan Miah <rayhanart94@yahoo.co.uk>
2020-05-07 17:11:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  const allEpisodes = getAllEpisodes();//creating a variable to getAllEpisodes
+  makePageForEpisodes(allEpisodes);//calling function passing another function
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  //rootElem.textContent = ${episodeList.id};
-}
+  const rootElem = document.getElementById("root");//getting root element from HTML page
+  rootElem.textContent = `Got ${episodeList.length} episode(s)`;//
+  episodeList.forEach((episodeList) => {
+     let container = document.createElement("div");
+     container.className = "column";
+     rootElem.appendChild(container);
+     let name = episodeList.name;
+     let number = episodeList.number;
+    let image = episodeList.image.medium;
+    let summary = episodeList.summary;
+
+    let h1El = document.createElement("h1");
+    container.appendChild(h1El);
+    h1El.textContent = name + number;
+
+    let elementImg = document.createElement("img");
+    container.appendChild(elementImg);
+    elementImg.src = image;
+
+    let elementSummary = document.createElement("p");
+    container.appendChild(elementSummary);
+    elementSummary.innerHTML = summary;
+
+  });
+
+   
+  }
+
 
 window.onload = setup;
