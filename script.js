3961f90ccc908cc3e88f385a9431c638bc3fb564
Rayhan Miah <rayhanart94@yahoo.co.uk>
2020-05-14 16:07:46 +0100

    fetch

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 //You can edit ALL of the code here
-function setup() {
+function setup() {//function for setting up api/function with json data
   const allEpisodes = getAllEpisodes();//creating a variable to getAllEpisodes
   makePageForEpisodes(allEpisodes);//calling function passing another function
 }
 
-function makePageForEpisodes(episodeList) {
+function makePageForEpisodes(episodeList) {//function to loop through all elements 
   const rootElem = document.getElementById("root");//getting root element from HTML page
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;//
-  episodeList.forEach((episodeList) => {
-     let divElement = document.createElement("div");
-     divElement.className = "column";
-     rootElem.appendChild(divElement);
-     let name = episodeList.name;
-     let season = episodeList.season;
-     let number = episodeList.number;
-    let image = episodeList.image.medium;
-    let summary = episodeList.summary;
+  rootElem.textContent = `Got ${episodeList.length} episode(s) and data has (originally) come from TVMaze.com`;//displaying how many episodes
+  episodeList.forEach((episodeList) => {//using foreach to manipulate the episode.js json file
+     let divElement = document.createElement("div");//create div element to put all elements here
+     divElement.className = "column";//to put it in a column
+     rootElem.appendChild(divElement);//div element gets added to the root so can add the json elements to it
+     let name = episodeList.name;//variable to get the name
+     let season = episodeList.season;//variable to get the season
+     let number = episodeList.number;//variable to get the number
+    let image = episodeList.image.medium;//variable to get the medium
+    let summary = episodeList.summary;//variable to get the summary
 
-    let h1El = document.createElement("h1");
-    divElement.appendChild(h1El);
-    h1El.textContent = name;
+    let h1El = document.createElement("h1");//create a h1 element
+    divElement.appendChild(h1El);//on the divelement append the h1
+    h1El.textContent = name;//putting name on it
 
     let elementSeason = document.createElement("h1");
     divElement.appendChild(elementSeason);
@@ -39,6 +39,17 @@ function makePageForEpisodes(episodeList) {
 
    
   }
+  //this makes a promise to this tvmaze (fresh prince nice tv show)
+  fetch("https://api.tvmaze.com/shows/582/episodes")
+     .then((response) => {
+    console.log("response");
+    console.log(response);//seeing what the response looks like
+    return response.json();//api is json format
+  })
 
+  .then((jsonData) => {
+    console.log("data found");
+    console.log(jsonData);
+});
 
 window.onload = setup;
